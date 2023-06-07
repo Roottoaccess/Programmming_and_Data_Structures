f33645edf290404146c1523414e8f7e3d76288c5
network_crab <network123crab@gmail.com>
2023-06-07 13:26:02 +0530

    variables in c++

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	// creating a variable
+	int x;
+	x = 5;
+	int y = x + 5;
+	// printing the value of the variable x
+	cout << "The value of the variable is: " << x;
+	// It is used for printing the new value in the next line
+	cout << "\n"; 
+	// After adding 5 with the original value the result is
+	cout << "The value after changing is: " << y;
+	return 0;
+}
